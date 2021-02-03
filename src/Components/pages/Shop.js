c562902d9c9bf6d7f7a51298f9d8064e1f639d82
Cody Neal <crn23@txstate.edu>
2021-02-03 16:20:29 -0600

    Fixes bug that prevented cart icon from updating

--- src/Components/pages/Shop.js
+++ src/Components/pages/Shop.js
@@ -1,82 +1,124 @@
-import React  from 'react'
-import { useState, useEffect } from 'react';
-import '../../App.css';
-import Products from './StoreComponents/Products/Products'
-import {Typography} from '@material-ui/core'
-import Cart from '../Cart/Cart';
-import { commerce } from '../../lib/commerce'
-import {BrowserRouter as Router, Switch,Route } from 'react-router-dom';
+import {React, useState} from 'react'
+import {Card, CardMedia, CardContent, CardActions, Typography, IconButton, InputLabel, Select, MenuItem} from '@material-ui/core'
+import {AddShoppingCart} from '@material-ui/icons'
+import useStyles from "./styles"
 
 
 
-
-
-const Shop = () => {
-
+const Product = ({product, onAddToCart }) => {
+    const classes = useStyles();
 
     
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState({});
-  
+  const [sizeSelection, setSizeSelection] = useState({});
 
-    const fetchProducts = async () => {
-        const { data } = await commerce.products.list();
-    
-        setProducts(data);
-      };
-
-      const fetchCart = async () => {
-
-        setCart(await commerce.cart.retrieve());
-      };
-
-      const handleAddToCart = async (productId,quantity,variant) => {
-        const { cart } = await commerce.cart.add(productId,quantity,variant);
-    
+    function buttonHandle(variantID,optionID,optQnty) {
         
-        setCart(cart);
-      };
+        
+        let variant = {
+          
+        [variantID]: optionID,
+        
+        }
 
-   
-      useEffect(() => {
-        fetchProducts();
-        fetchCart();
-
-      }, []);
+       
+      
+       console.log(optQnty)
+      if(optQnty <= 0 ){
+          alert("We're sorry, this option is currently sold out.")
+      }
+      else{
+        onAddToCart(product.id,1,variant);
+      }
+       
+      }
+      
+    
 
       
+     function setSize(variantID, optionID,optQnty){
+
+        
+        buttonHandle(variantID,optionID,optQnty);
+        
       
-   
+      }
+
+    
+    function triggerReload(){
+
+        window.location.reload(false);
+    }
+      
+      
+    
+     
+    
+    
     return (
+      
 
-       <Router>
-       
-            <div>
-                <Switch>
+        <Card className={classes.root} >
+            <CardMedia className={classes.media} image={product.media.source} title={product.name}/>
 
-                    <Route exact path="/shop">
-                      <Products products={products} onAddToCart={handleAddToCart}/> 
+            <CardContent>
+                <div className={classes.cardContent}>
 
-                     {/* 
-                      //Shop maintenance msg for changes to be made while site during production 
-                      <Typography variant="h3" color="textSecondary"style={{backgroundColor:'white', opacity:'100%'}} >
-                        <br/> <br/>
-                        Sorry, our shop is currently down for maintenance. <br/>
-                        Please check back soon!
-                        <br/> <br/> <br/> <br/>
-                      </Typography>
-                   */}
+                    <Typography variant='h5' gutterBottom>
+                        {product.name}
+                    </Typography>
 
-                    </Route>
+                    {product.variants.map((variant) => (
+                    
+                    <Typography variant='h5'>
+                        {variant.options[0].price.formatted_with_symbol}
+                    </Typography>
 
-                </Switch>
+                   
+                          
+                    ))}
+                </div>
                 
-           
-            </div>
+                <Typography dangerouslySetInnerHTML={{__html: product.description}}  variant='body2' color='inherit' style={{backgroundColor:'white', opacity:'100%'}}/>
+                   <br/> <br/>
+                   <InputLabel> Size Selection </InputLabel>
+                   <ul>
+                     {product.variants.map((variant) => (
+                      <Select value={sizeSelection} fullWidth onChange={(e) => setSizeSelection(e.target.value)}>
+                         
+                           
+                             { variant.options.map((option) => (
 
-        </Router>
-       
+                              <MenuItem key={option.id} value={option.id} onClick={() => setSize(variant.id,option.id,option.quantity)} >
+                                {option.name}
+                              </MenuItem>
+                                 
+                             ))
+                            } 
+                           
+                       
+                      </Select>
+
+                       
+
+                     ))}
+                     
+                   </ul>
+
+                   
+
+            </CardContent>
+
+            <CardActions disableSpacing className={classes.cardActions}  onClick={() => triggerReload()}>
+                <IconButton aria-label='Add to Cart' >
+                    <AddShoppingCart/>
+
+                </IconButton>
+            </CardActions>
+
+
+            
+            </Card>
     )
 }
 
-export default Shop
+export default Product
